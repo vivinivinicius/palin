8a0cc718a578a4e70d4ea4dfd934dbeb96fb03ce
Vinicius <legithacking069@gmail.com>
2024-03-17 20:14:27 -0300

    Added palindrome checker & Added palindrome visual part

--- src/app/palin/palin.component.ts
+++ src/app/palin/palin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-palin',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './palin.component.css'
 })
 export class PalinComponent {
+
+  inputText: string = '';
+  umPalindromo: boolean | null = null;
+
+  verificadorPalindromo(): void {
+    const inputLimpo = this.inputText.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const inputReverso = inputLimpo.split('').reverse().join('');
+    this.umPalindromo = inputLimpo === inputReverso;
+  }
 
 }
